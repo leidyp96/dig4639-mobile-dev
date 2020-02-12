ba1197b2542e16e48b5b63a0669bc56d534fecbd
leidyp96 <59659047+leidyp96@users.noreply.github.com>
2020-02-12 11:43:43 -0500

    Updates

---
+++ precache-manifest.aecdc8bff28cd6db595d08c7b05e8cb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a5cfb26c70d76d7d46b7533d2c0d9bd",
+    "url": "/dig4639-mobile-dev/index.html"
+  },
+  {
+    "revision": "f5c8dbaf1fac0ba8b8ee",
+    "url": "/dig4639-mobile-dev/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "e02cba94be16e740280c",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/dig4639-mobile-dev/static/js/2.3ded2b1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5c8dbaf1fac0ba8b8ee",
+    "url": "/dig4639-mobile-dev/static/js/main.3650d8a0.chunk.js"
+  },
+  {
+    "revision": "170169ccdc7dff24daa3",
+    "url": "/dig4639-mobile-dev/static/js/runtime-main.e7bb69b4.js"
+  }
+]);
