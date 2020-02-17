70088fbf81d9817a745c47261f7dd70d2b120e71
leidyp96 <59659047+leidyp96@users.noreply.github.com>
2020-02-17 12:23:27 -0500

    ICE-7: Jest Testing

--- ice/w7a/MyMath.js
+++ ice/w7a/MyMath.js
@@ -1,3 +1,36 @@
-export function Sum (a, b) {
-  return a + b
+ function Sum (a, b) {
+   let result = undefined
+ if(typeof a == "number" && typeof b == "number") {
+  result =  a + b
+ } 
+  return result;
 }
+
+
+
+
+function AddList (arr){
+  let result = undefined;
+  if (Array.isArray(arr) && arr.length>0){
+    result = 0;
+    for(var i = 0;i < arr.length; i++){
+      if (typeof = arr[i] != "number"){
+        result = undefined;
+        break;
+      }
+      result = result + arr[i];
+  }
+}
+  return result;
+}
+function DivideBy (){
+
+}
+
+function ContainsString (){
+
+}
+function ReSortedNumbers (){
+
+}
+export { Sum,AddList};
--- ice/w7a/MyMath.test.js
+++ ice/w7a/MyMath.test.js
@@ -1,9 +1,38 @@
-import { Sum } from './sum.js'
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(Sum(1, 2)).toBe(3)
+import { Sum, AddList } from './MyMath.js'
+describe("AddList", () => {
+  test('Adds a lsit of numbers together', () => {
+    let input = [1,3,4,5];
+    let result = 13;
+    expect(AddList(input)).toBe(result);
+    input = [15,5,8,-10];
+    result = 18;
+    expect(AddList(input)).toBe(result);
+  })
+  test('test if empty array returns undefined', () => {
+    let input = []
+    expect(AddList(input)).toBeUndefined();
+  })
+  test('test if empty array returns undefined', () => {
+    let input = [undefined,0,2,3]
+    expect(AddList(input)).toBeUndefined();
+  })
+  test('test if non-array returns indefined', () => {
+    let input = 100;
+    expect(AddList(input)).toBeUndefined();
+  })
 })
 
+describe ("Sum", () => {
+  test('Whether undefined is returned on invalid type', () => {
+    expect(Sum(1, "Test")).toBeUndefined()
+  })
+  test('adds 1 + 2 to equal 3', () => {
+    expect(Sum(1, 2)).toBe(3)
+  })
+  it('produces the sum of 10 + 20, which should be 30', () => {
+    expect(Sum(10, 20)).toBe(30)
+  })
+})
 // ICE 
 
 // Examples for Null
